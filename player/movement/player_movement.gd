44a78e6feb5ed23dd30fe892068b08f5fd7d009c
LisaGaydamaka <91667842+LisaGaydamaka@users.noreply.github.com>
2026-09-07 20:17:46 +0300

    Expose physical movement contacts

--- player/movement/player_movement.gd
+++ player/movement/player_movement.gd
@@ -2,6 +2,9 @@ class_name PlayerMovement
 extends RefCounted
 
 
+const MOTION_EPSILON_SQUARED: float = 0.000001
+
+
 var max_collision_iterations: int
 var step_up: PlayerStepUp
 
@@ -20,7 +23,8 @@ func move(
 	input_direction: Vector3,
 	allow_step_up: bool,
 	delta: float
-) -> void:
+) -> Array[KinematicCollision3D]:
+	var collisions: Array[KinematicCollision3D] = []
 	var motion: Vector3 = player.velocity * delta
 	var horizontal_motion: Vector3 = Vector3(
 		motion.x,
@@ -49,7 +53,7 @@ func move(
 	for _iteration: int in range(
 		max_collision_iterations
 	):
-		if motion.length_squared() <= 0.000001:
+		if motion.length_squared() <= MOTION_EPSILON_SQUARED:
 			break
 
 		var collision: KinematicCollision3D = (
@@ -58,6 +62,7 @@ func move(
 
 		if collision == null:
 			break
+		collisions.append(collision)
 
 		var normal: Vector3 = collision.get_normal()
 		var remainder: Vector3 = collision.get_remainder()
@@ -74,3 +79,27 @@ func move(
 			player.velocity -= normal * normal_velocity
 
 		motion = remainder.slide(normal)
+
+	return collisions
+
+
+func move_vertical_velocity(
+	player: CharacterBody3D,
+	delta: float
+) -> void:
+	var motion: Vector3 = Vector3.UP * player.velocity.y * delta
+
+	for _iteration: int in range(max_collision_iterations):
+		if motion.length_squared() <= MOTION_EPSILON_SQUARED:
+			break
+
+		var collision: KinematicCollision3D = player.move_and_collide(motion)
+		if collision == null:
+			break
+
+		var normal: Vector3 = collision.get_normal()
+		var normal_velocity: float = player.velocity.dot(normal)
+		if normal_velocity < 0.0:
+			player.velocity -= normal * normal_velocity
+
+		motion = collision.get_remainder().slide(normal)
